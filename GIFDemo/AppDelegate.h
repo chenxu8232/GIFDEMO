3b13180d319f6c1c9ea1bb601c577f3ff4cc5701
Xu Chen <xuchen@XudeiMac.local>
2017-05-25 10:56:28 +0800

    Initial Commit

---
+++ GIFDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIFDemo
+//
+//  Created by Xu Chen on 2017/5/25.
+//  Copyright © 2017年 Xu Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GIFDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GIFDemo
+//
+//  Created by Xu Chen on 2017/5/25.
+//  Copyright © 2017年 Xu Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
